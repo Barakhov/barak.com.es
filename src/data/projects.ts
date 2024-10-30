4d6d7b9f8311697e9753b199bdfea4a19b9fc2cc
Alex Barakhov <alex.barakhov@tui.com>
2024-10-30 13:41:58 +0100

    cosas

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -25,7 +25,32 @@ const projects: Project[] = [
     title: "Textualize",
     techs: ["Eleventy", "JavaScript", "CSS3"],
     link: "https://www.textualize.io/",
-  },  
+  },
+  {
+    title: "Redradix",
+    techs: ["Webflow", "GSAP"],
+    link: "https://www.redradix.com/",
+  }, 
+  {
+    title: "Ambar Partners",
+    techs: ["Webflow", "GSAP"],
+    link: "https://www.ambarpartners.com/",
+  }, 
+  {
+    title: "Fundación Luckia",
+    techs: ["Webflow", "GSAP", "Matter.js"],
+    link: "https://www.fundacionluckia.com/",
+  }, 
+  {
+    title: "Vent Finance",
+    techs: ["Webflow"],
+    link: "https://www.vent.finance/",
+  }, 
+  {
+    title: "Electromaps",
+    techs: ["Webflow"],
+    link: "https://www.electromaps.com/",
+  },          
 ];
 
 export default projects;
