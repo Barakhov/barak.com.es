39f34b97900b7ed636087369b3b8d219a503941e
Alex Barakhov <alex.barakhov@tui.com>
2024-11-08 17:11:46 +0100

    added theme toggle

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,11 +2,12 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Hanken Grotesk Variable", ...defaultTheme.fontFamily.sans],
+        sans: ["Manrope Variable", ...defaultTheme.fontFamily.sans],
       },
     },
   },
